db59b55a1d26ae1017372119b83ea7ab5b9ee968
Petr Simon <devnull@localhost>
2010-02-24 06:14:33 +0000

    version 1.6.7

--- defaults/preferences/lyz.js
+++ defaults/preferences/lyz.js
@@ -1,3 +1,4 @@
 pref("extensions.lyz.lyxserver", "\\\\.\\pipe\\lyxpipe");
 pref("extensions.lyz.citekey", "author year title");
-
+//pref("extensions.lyz.kile", false);
+pref("extensions.lyz.use_utf8", false);
